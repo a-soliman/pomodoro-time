b277b89b2a86f032a2346916fb23cfd66bea4a25
Ahmed Soliman <ahmed.soliman@programmer.net>
2018-01-23 18:37:09 -0800

    coded display time function

--- playground/app.js
+++ playground/app.js
@@ -3,12 +3,8 @@ let seconds = 1;
 let minutes	= 0;
 let hours	= 0;
 
-let shortBreak = 3;
-
-let longBreak = {
-	minutes: 10,
-	seconds: 0
-};
+let shortBreak 	= 300;
+let longBreak 	= 600;
 
 /*
 setInterval(()=> {
@@ -30,25 +26,51 @@ setInterval(()=> {
 }, 1000)
 */
 
-let startShortBreak = setInterval(function() { 
+// let startShortBreak = setInterval(() => { 
 	
-	myTimer();
+// 	myTimer();
 
-	if( shortBreak === 0) {
-		stopTimer(startShortBreak);
+// 	if( shortBreak === 0) {
+// 		stopTimer(startShortBreak);
 
-	}
-}, 1000);
+// 	}
+// }, 1000);
+
+let stopTimer = ( timer ) => {
+	clearInterval(timer);
+}	
 
 function myTimer() {
     console.log(shortBreak);
     shortBreak--
 }
 
+let displayTime = ( time ) => {
+	let hours = 0, min = 0, sec = 0;
 
-let stopTimer = ( timer ) => {
-	clearInterval(timer);
-}	
+	while( time > 0) {
+		if ( time >= 3600 ) {
+			hours++;
+			time = time - 3600;
+		}
+		if (time >= 60) {
+			min++;
+			time = time - 60;
+		}
+		else {
+			sec = time;
+			time = 0;
+		}
+	}
+
+	return `HOURS : ${hours}, MIN : ${min}, SEC : ${sec}`;
+	
+}
+
+console.log(displayTime(125))
+
+
+
 
 
 
